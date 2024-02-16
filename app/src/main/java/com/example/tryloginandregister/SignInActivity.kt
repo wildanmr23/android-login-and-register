d4860a11f40f05e8cd2b14e8aa1aab9e64e66ef0
wildanmr23 <wildanmursalin.28@gmail.com>
2024-02-16 08:09:15 +0700

    Finished Project

---
+++ app/src/main/java/com/example/tryloginandregister/SignInActivity.kt
@@ -0,0 +1,45 @@
+package com.example.tryloginandregister
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tryloginandregister.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
